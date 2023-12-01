d98c28d46f248096b1daedaca08ff19680573c88
szabomarcel <szabomarcell34@gmail.com>
2023-12-01 19:50:15 +0100

    login

--- FormTermek.Designer.cs
+++ FormTermek.Designer.cs
@@ -114,6 +114,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Location = new System.Drawing.Point(9, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(425, 23);
